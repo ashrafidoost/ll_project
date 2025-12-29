c200bb6564558e4a834a80c8ac79bb41e9bac2a7
Reza Ash <reza.ashrafidoost@gmail.com>
2025-12-28 22:02:40 -0400

    Ready to deployment to Platform.sh.

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+''' Defines url patterns for accounts. '''
+from django.urls import path, include
+from . import views
+
+app_name = 'accounts'
+urlpatterns = [
+    # Include defaut auth urls.
+    path('', include('django.contrib.auth.urls')),
+    # Registration page.
+    path('register/', views.register, name='register'),
+]
